e53d04fac91a538828f6f375c38eeaeb6a70d77a
Cor Zuurmond <jczuurmond@protonmail.com>
2021-12-22 16:21:25 +0100

    Add ingest section to warehouse yml parser

--- core/sodasql/scan/warehouse_yml_parser.py
+++ core/sodasql/scan/warehouse_yml_parser.py
@@ -20,6 +20,7 @@ from sodasql.scan.warehouse_yml import WarehouseYml
 KEY_NAME = 'name'
 KEY_CONNECTION = 'connection'
 KEY_SODA_ACCOUNT = 'soda_account'
+KEY_INGEST = 'ingest'
 
 SODA_KEY_HOST = 'host'
 SODA_KEY_PORT = 'port'
@@ -27,7 +28,9 @@ SODA_KEY_PROTOCOL = 'protocol'
 SODA_KEY_API_KEY_ID = 'api_key_id'
 SODA_KEY_API_KEY_SECRET = 'api_key_secret'
 
-VALID_WAREHOUSE_KEYS = [KEY_NAME, KEY_CONNECTION, KEY_SODA_ACCOUNT]
+DBT_CLOUD_KEY_API_TOKEN = "dbt_cloud_api_token"
+
+VALID_WAREHOUSE_KEYS = [KEY_NAME, KEY_CONNECTION, KEY_SODA_ACCOUNT, KEY_INGEST]
 
 logger = logging.getLogger(__name__)
 
@@ -77,6 +80,10 @@ class WarehouseYmlParser(Parser):
                 self.warehouse_yml.soda_api_key_id = self.get_str_required_env(SODA_KEY_API_KEY_ID)
                 self.warehouse_yml.soda_api_key_secret = self.get_str_required_env(SODA_KEY_API_KEY_SECRET)
                 self._pop_context()
+
+            ingest_dict = self.get_dict_optional(KEY_INGEST)
+            if ingest_dict:
+                self.warehouse_yml.dbt_cloud_api_token = self.get_str_optional(DBT_CLOUD_KEY_API_TOKEN)
 
             self.check_invalid_keys(VALID_WAREHOUSE_KEYS)
 
